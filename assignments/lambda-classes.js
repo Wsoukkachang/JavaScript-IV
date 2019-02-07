f8a7652913c9ac7d862b910e078c3370f5d3a360
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-06 23:08:00 -0600

    added student and PM classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,3 +26,39 @@ class Instructor extends Person {
         return `${student.name} receives a perfect score on ${subject}`;
     }
 }
+
+class Student extends Person {
+    constructor (childAttributes) {
+        super(childAttributes);
+        this.previousBackground = childAttributes.previousBackground;
+        this.className = childAttributes.className;
+        this.favSubjects = childAttributes.favSubjects;
+    }
+    listsSubjects() {
+        return `${this.favSubjects}`;
+    }
+
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+
+    printChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+class ProjectManagers extends Instructor {
+    constructor (gchildAttributes) {
+        super(gchildAttributes);
+        this.gradClassName = gchildAttributes.gradClassName;
+        this.favInstructor = gchildAttributes.favInstructor;
+        this.favSubjects = gchildAttributes.favSubjects;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
+    }
+
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
