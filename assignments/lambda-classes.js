7a9d01375883d29e24c2027c2583087f10229e7b
Wsoukkachang <Wsoukkachang@yahoo.com>
2019-02-06 22:58:42 -0600

    added Person and Instructor classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,28 @@
 // CODE here for your Lambda Classes 
+
+class Person {
+    constructor (attributes) {
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+      }
+    }
+
+class Instructor extends Person {
+    constructor (childAttributes) {
+        super(childAttributes);
+        this.specialty = childAttributes.specialty;
+        this.favLanguage = childAttributes.favLanguage;
+        this.catchPhrase = childAttributes.catchPhrase;
+    }
+    demo() {
+        return `Today we are learning about ${subject}`;
+    }
+
+    grade() {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
